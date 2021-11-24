aa101cd330fb9133fa78b9fa21903f4107d9f1a0
Ana <anahaydn3@gmail.com>
2021-11-23 22:57:03 -0300

    adicionando json teste

--- ana.js
+++ ana.js
@@ -1,54 +1,51 @@
+/*
 async function repasseChart() {
-    await getRepasseData()
-
-    const ctx = document.getElementById('line-chartTartaruga').getContext('2d');
-
-        const chart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: anoData,
-                datasets: [{
-                    labels:esferaData,
-                    datasets: valorTotalData,
-                    backgroundColor:'rgba(2,33,36,0.6)',
-                    borderColor: 'rgba(2,33,36,0.6)',
-
-                    }
-                ]
-            }
-        })
-    
-}
-
-
-async function repasseChart() {
-    await getRepasseData()
+    await getRepasseData()*/
 
     const ctx = document.getElementById('line-chartVitoria').getContext('2d');
 
         const chart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: [2018,2019,2020,2021],
+                labels: [1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010.2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021],
                     datasets: [
                         {
                             label: "Creche",
-                            data: [1,2,3,4],
+                            data: [1,2,3,4,5,6,7,7,8,6,7,3,4,6,1,3,5,7,4,3,8,9],
                             backgroundColor:'transparent',
                             borderColor: 'rgba(2,33,36,0.6)',
                         },
                         {
-                            label: "Ensino Fundamental",
-                            data: [4,7,6,2],
+                            label: "Especializada",
+                            data: [4,7,6,2,3,2,5,1,7,8,9,4,5,3,7,6,8,4,7,2],
                             backgroundColor:'transparent',
                             borderColor: 'rgba(6,243,16,0.6)',
-                        }
+                        },
+                        {
+                            label: "AEE",
+                            data: [1,2,3,4,5,6,7,7,8,6,7,3,4,6,1,3,5,7,4,3,8,9],
+                            backgroundColor:'transparent',
+                            borderColor: 'rgba(2,33,36,0.6)',
+                        },
+                        {
+                            label: "Eja",
+                            data: [1,2,3,4,5,6,7,7,8,6,7,3,4,6,1,3,5,7,4,3,8,9],
+                            backgroundColor:'transparent',
+                            borderColor: 'rgba(2,33,36,0.6)',
+                        },
+                        {
+                            label: "Medio",
+                            data: [1,2,3,4,5,6,7,7,8,6,7,3,4,6,1,3,5,7,4,3,8,9],
+                            backgroundColor:'transparent',
+                            borderColor: 'rgba(2,33,36,0.6)',
+                        },
+                    
                      ],
             }
         });
-    
+/* 
 }
-
+*/
 repasseChart()
 
 async function getRepasseData(){
@@ -61,8 +58,23 @@ async function getRepasseData(){
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'      
           }});
-    const barChartData = await response.json();
-    
+    const data = await response.json();
+console.log(data.Macapa[0])
+
+
+    /*
+    console.log(data[2018][0].Macapa)
+    console.log(data[2019][0].Macapa)
+    console.log(data[2020][0].Macapa)
+    console.log(data[2021][0].Macapa)
+//*
+   // console.log(data.municipio[0].macapa.ano)
+/*
+    const ano = data.municipio[0].macapa.ano[0][2018][0]
+    anoData = ano
+    console.log(ano[0][2018][0])
+    /*
+    /*
     const ano = barChartData.content.map((x) => x.ano)
     const esfera = barChartData.content.map((x) => x.esferaGoverno)
     const valorTotal = barChartData.content.map((x) => x.valorTotalEscolas)
@@ -77,5 +89,5 @@ async function getRepasseData(){
     valorTotalData = valorTotal
     console.log(valorTotalData)
     
-    
+    */
 }
